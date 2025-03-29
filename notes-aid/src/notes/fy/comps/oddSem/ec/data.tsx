4285c38e84fabe5d99709a12f8a08c32e9b8fee7
Sai Bhadane <84670047+sb-byte@users.noreply.github.com>
2025-03-29 22:41:33 +0530

    Added EC Notes (#55)

--- notes-aid/src/notes/fy/comps/oddSem/ec/data.tsx
+++ notes-aid/src/notes/fy/comps/oddSem/ec/data.tsx
@@ -6,10 +6,327 @@ const ec = {
   color: "blue",
   modules: {
     1: {
-      notesLink: [],
-      topics: [],
+      notesLink: [
+        {
+          title: "1.1-1.2 Introduction-Treatment-BOD COD",
+          url:"https://drive.google.com/file/d/1XdDvUelji5f_eZ_UD_6IdfhN1ZuvTdoW/view?usp=drive_link",
+        },
+        {
+          title: "1.3 AI and IoT",
+          url:"https://drive.google.com/file/d/1dU-fFiMsy1TvFR84kU4mbkit6isSOjh2/view?usp=drive_link",
+        }
+      ],
+      topics: [
+        {
+          title: "1.1 Introduction, Types of Hardness",
+          description: "Introduction to Water Chemistry",
+          videos: [
+            {
+              title: "Water and it's importance",
+              url: "https://www.youtube.com/embed/CVK0hzlPI3k?si=tkBtYAT5Q_vVq70E"
+            },
+            {
+              title: "Impurities in Water",
+              url: "https://www.youtube.com/embed/TxVurV7Qmsk?si=vzkp-Z4IUtyr6gkm"
+            },
+            {
+              title: "Hardness of Water",
+              url: "https://www.youtube.com/embed/mnx7kfL4f38?si=3eBdF3zBCqJ93VSr"
+            },
+            {
+              title: "Unit of Hardness",
+              url: "https://www.youtube.com/embed/E5I94wWw_bc?si=HDIKJdW-yz2AbFac"
+            },
+            {
+              title: "Hardness Multiplying Factor",
+              url: "https://www.youtube.com/embed/wBbbggoyCNs?si=q-sfllZD3u-pBalF"
+            }
+          ]
+        },
+        {
+          title: "1.2 Water Treatment, BOD & COD",
+          description: "Water Treatment: Lime soda method Zeolite method, Ion Exchange process, BOD & COD",
+          videos: [
+            {
+              title: "EDTA Method Part 1",
+              url: "https://www.youtube.com/embed/YHAgyxt7QcM?si=cs9e175XVk_gZXEh" 
+            },
+            {
+              title: "EDTA Method Part 2",
+              url: "https://www.youtube.com/embed/vNwcJPEhHPA?si=hv9LBm1uNvxIw76-" 
+            },
+          {
+              title: "Softening of Water",
+              url: "https://www.youtube.com/embed/L5RTA14XekQ?si=ct7mM4a630byi42L" 
+            },
+            {
+              title: "Caustic Embrittlement in Boiler",
+              url: "https://www.youtube.com/embed/A3TcAl9SRzM?si=grEYRsmvYMpRXZeo" 
+            },
+            {
+              title: "Lime Soda Process",
+              url: "https://www.youtube.com/embed/CcMApIy2tz0?si=ZivxVv9r8PqhQyGc" 
+            },
+            {
+              title: "Zeolite Process",
+              url: "https://www.youtube.com/embed/nIvMJqGkN9M?si=xXko62VU9YbGzcko" 
+            },
+            {
+              title: "Ion Exchange Process",
+              url: "https://www.youtube.com/embed/Z99ll9KTXug?si=oqvFuot9ED64XqoX" 
+            },
+          ]
+        },
+        {
+          title: "1.3 AI and IoT",
+          description: "Artificial Intelligence & Internet of Things in Water Management",
+          videos: []
+        }
+      ],
     },
+  
+  2: {
+    notesLink: [
+      {
+        title: "2.1 Polymers",
+        url:"https://drive.google.com/file/d/1VCNEzvR7xzYXbaWEAZu1SxTZLE1LsuqB/view?usp=drive_link",
+      },
+      {
+        title: "2.2 Nanomaterials",
+        url:"https://drive.google.com/file/d/1R0f9ucIB-BSt5peQtyo8z131y239dR5j/view?usp=drive_link",
+      },
+      {
+        title: "2.3 Biomaterials",
+        url:"https://drive.google.com/file/d/1RtrHSYXK_4_yfQ9ktsyb1e2bS2O9Wvwd/view?usp=drive_link",
+      },
+      {
+        title: "2.4 MEMS and Microsystems",
+        url:"https://drive.google.com/file/d/1ECR6oWaeuVgrRhiSy-wnRIcZBiSdx25e/view?usp=drive_link",
+      }
+    ],
+    topics: [
+      {
+        title: "2.1 Polymers",
+        description: "Polymers as Industrials Materials, Conducting polymers, Fabrications of Polymers, Biodegradable Polymers",
+        videos: [
+          {
+            title: "Introduction to Polymers",
+            url: "https://www.youtube.com/embed/C0rArqoqsBE?si=Eo7wbkC2LO0Jqkpn" 
+          },
+          {
+            title: "Classification of Polymers",
+            url: "https://www.youtube.com/embed/CUculXMKAuA?si=rjDqXMEzIt2V9kPF" 
+          },
+          {
+            title: "Addition polymer",
+            url: "https://www.youtube.com/embed/H3nWKkF2-ec?si=WUlGtuX1qzPbBj3K" 
+          },
+          {
+            title: "Thermoplastics and Thermosetting Plastics",
+            url: "https://www.youtube.com/embed/JAcOrPZzbmw?si=bQ_a6zPmqLYWO4WD" 
+          },
+          {
+            title: "Condensation and Addition polymer",
+            url: "https://www.youtube.com/embed/Vuk6pJGSzvE?si=ffAZ5D0Dz9mRJ1V2" 
+          },
+          {
+            title: "Biodegradable Polymers",
+            url: "https://www.youtube.com/embed/yE5LKznnoCw?si=LEcsRPXi3G9I5Ic5" 
+          },
+          {
+            title: "Vulcanization of Rubber",
+            url: "https://www.youtube.com/embed/0w8L4qv1B5U?si=8rVxXkX3gQqY1i2i" 
+          },
+        ]
+      },
+      {
+        title: "2.2 Nanomaterials",
+        description: " Introductions, Classifications, Growth Techniques for Nanomaterials, Applications",
+        videos: [
+          {
+            title: "Introduction to Nanomaterials",
+            url: "https://www.youtube.com/embed/eyOxvNTgGgs?si=er7DoMe6MkPMdKe2" 
+          },
+          {
+            title: "Carbon Nanotubes",
+            url: "https://www.youtube.com/embed/5wA9sShMVJw?si=68zYur1JKj9FGwYH" 
+          },
+        ]
+      },
+        {
+          title: "2.3 Biomaterials",
+          description: "Metals & Alloys, Bio Ceramics, Composites, new materials in prosthetics",
+          videos: []
+        },
+        {
+          title: "2.4 MEMS and Microsystems",
+          description: " Introduction, Active substrate materials, Silicon as substrate materials",
+          videos: []
+        }
+    ],
   },
+  3: {
+    notesLink: [
+      {
+        title: "3.1 Energy & Sustainable Development",
+        url:"https://drive.google.com/file/d/1WPMKuZMQBOzdSJQoB84Nku4p9fZuNmHS/view?usp=drive_link",
+      },
+      {
+        title: "3.2 Hydrocarbon as Fuel, Bio Diesel, Power Alcohol",
+        url:"https://drive.google.com/file/d/1B5AzQkkB4KX0DGw62dI-DD7EifTHzr4W/view?usp=drive_link",
+      },
+      {
+        title: "3.3 Rechargeable Batteries",
+        url:"https://drive.google.com/file/d/1IOSp0oCebThohhUyhzaADXld970CeyLV/view?usp=drive_link",
+      }
+    ],
+    topics: [
+      {
+        title: "3.1 Energy & Sustainable Development",
+        description: "Renewable Energy, Solar Energy, solar Photovoltaic, solar heater",
+        videos: [
+          {
+            title: "Fuels and Properties",
+            url: "https://www.youtube.com/embed/try_SCnEYeA?si=c4xQpYjMbHBdRiKs" 
+          },
+        ]
+      },
+      {
+        title: "3.2 Hydrocarbon as Fuel, Bio Diesel, Power Alcohol",
+        description: "Unit of Energy, characteristic of good fuel, Bio Diesel, Power Alcohol",
+        videos: [
+          {
+            title: "Calorific Value- Higher and Lower",
+            url: "https://www.youtube.com/embed/tio1eCnkIAA?si=JRZ-ljybvgDFuryT" 
+          },
+          {
+            title: "Numerical on Dulong's Formula",
+            url: "https://www.youtube.com/embed/qrC5jADuiME?si=5pX2xztJMNJk3DzE" 
+          },
+          {
+            title: "Proximate Analysis of Coal",
+            url: "https://www.youtube.com/embed/WST-H-n9mfY?si=KWSL0Ni7r-w5CiZI" 
+          },
+        ]
+      },
+      {
+        title: "3.3 Rechargeable Batteries",
+        description: "Lead acid battery, Lithium ion battery, Nickel based battery, other battery technology",
+        videos: []
+      },
+    ],
+  },
+  4: {
+    notesLink: [
+      {
+        title: "4.1- 4.2- 4.4 UV-IR Spectroscopy-Conductometry",
+        url:"https://drive.google.com/file/d/1l99mxI-CaZhbofvbp_cKfP7q9pFwHe5x/view?usp=drive_link",
+      },
+      {
+        title: "4.3 1H NMR Spectroscopy",
+        url:"https://drive.google.com/file/d/13BSsOW0wJVGYZv2Nbmf_lzZMUEc1fPbI/view?usp=drive_link",
+      },
+    ],
+    topics: [
+      {
+        title: "4.1 UV Spectroscopy",
+        description: "Principle, Instrumentation and application",
+        videos: [
+          {
+            title: "Introduction to Spectroscopy",
+            url: "https://www.youtube.com/embed/RaMg5avQIO0?si=NjLAawMpGx4cEOII" 
+          },
+          {
+            title: "Electromagnetic Radiation",
+            url: "https://www.youtube.com/embed/RX8JctkQB-M?si=XfK1GItF_FVVq-w3" 
+          },
+          {
+            title: "Spectroscopy and its types",
+            url: "https://www.youtube.com/embed/HqYDTuGupPo?si=dRKjvdZasvSdrySv" 
+          },
+          {
+          title: "UV Spectroscopy",
+            url: "https://www.youtube.com/embed/c5DhJyQh-QE?si=V4bf2HRsJXpnWiz3" 
+          },
+        ]
+      },
+      {
+        title: "4.2 IR Spectroscopy",
+        description: "Basic Principle, Instrumentation and applications",
+        videos: [
+          {
+            title: "Infrared Spectroscopy Part 1",
+            url: "https://www.youtube.com/embed/j1zZklPJcM8?si=qvhri3kCTHgMaATo" 
+          },
+          {
+            title: "Infrared Spectroscopy Part 2",
+            url: "https://www.youtube.com/embed/lQLL_mNWGyU?si=dLzmqV3BwiBJsHpK" 
+          },
+          {
+            title: "Molecular Vibrations in IR Spectroscopy",
+            url: "https://www.youtube.com/embed/BU27H9UC3lg?si=XVwq1eZwxFfGoG7E" 
+          },
+        ]
+      },
+      {
+        title: "4.3 1H NMR Spectroscopy",
+        description: "Principle, Instrumentation, Chemical Shift, Factors affecting chemical shift, Applications",
+        videos: [
+          {
+            title: "Nuclear Magnetic Resonance(NMR) Spectroscopy",
+            url: "https://www.youtube.com/embed/uYa19hpVYxU?si=b3vud9gQEh2Ma4sl" 
+          },
+          {
+            title: "Chemical Shift in NMR",
+            url: "https://www.youtube.com/embed/qnjW7zJ24Hk?si=FTMRulUJzZ2JMJgi" 
+          },
+          {
+            title: "Factors affecting Chemical Shift",
+            url: "https://www.youtube.com/embed/gGY52XWF0cM?si=BXS3RrKVLMntq_Av" 
+          },
+        ]
+      },
+      {
+        title: "4.4 Ph Metry, Conductometry",
+        description: "Electroanalytical techniques, pH-metry, Conductometry, Potentiometry",
+        videos: []
+      },
+    ],  
+  },
+  5: {
+    notesLink: [
+      {
+        title: "5.1 Computational Chemistry",
+        url:"https://drive.google.com/file/d/1zHE8O5_hC3ISAtvtg-Vu1cxKJBJhGlxl/view?usp=drive_link",
+      },
+      {
+        title: "5.2 Cheminformatics and Bioinformatics",
+        url:"https://drive.google.com/file/d/1Ttcg2ihRIIrmMxIvqN9AKdq4AwlkKeio/view?usp=drive_link",
+      },
+      {
+        title: "5.3 E-waste Management",
+        url:"https://drive.google.com/file/d/1CgBIBC9GsMl6QONFUKHzoSAJsQ6Ifg2-/view?usp=drive_link",
+      },
+    ],
+    topics: [
+      {
+        title: "5.1 Computational Chemistry",
+        description: "Software and Hardware, Applications of Computational Chemistry",
+        videos: []
+      },
+      {
+        title: "5.2 Cheminformatics and Bioinformatics",
+        description: "Computational approach in Cheminformatics and Bioinformatics",
+        videos: []
+      },
+      {
+        title: "5.3 E-waste Management",
+        description: ": Sustainable Development & e-waste management, impact of legislations on materials used in electronics",
+        videos: []
+      },
+    ],  
+  },
+},
 };
 
 export default ec;
