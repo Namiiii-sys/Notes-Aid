cb0c07df0fbfe08cf695a6c4bb514fdea3191682
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-03-31 05:36:10 +0530

    added ms and df, IT dept TY 1-3 modules. no build error. gonna add the notes next.

--- notes-aid/src/notes/ty/it/evenSem/df/data.tsx
+++ notes-aid/src/notes/ty/it/evenSem/df/data.tsx
@@ -6,15 +6,58 @@ const df = {
   icon: Brain,
   color: "blue",
   modules: {
-   1:{
-    notesLink:[
-
-    ],
-    topics:[
-
-    ]
-   }
+    1: {
+      notesLink: [
+        {
+          title: "Notes 1",
+          url: "https://firebasestorage.googleapis.com/v0/b/acm-bruh.appspot.com/o/AC%20Module%201.pdf?alt=media&token=42db56c3-72f1-4758-acb8-8f3ecab36ff3",
+        },
+      ],
+      topics: [
+        {
+          title: "1.1 Basics of Frameworks and Introduction to Laravel ",
+          description: "Theoretical based concepts- Important for exam ⭐",
+          videos: [
+            {
+              title: "Laravel in 100 Seconds",
+              url: "https://www.youtube.com/embed/rIfdg_Ot-LI?si=5fI4vqV8cMnx4utN",
+            },
+          ],
+        },
+        {
+          title: "•	1.2 Laravel",
+          description: "Theoretical based concepts- Important for exam ⭐",
+          videos: [
+            {
+              title: "Laravel Tutorial For Beginners (Simple User CRUD App)",
+              url: "https://www.youtube.com/embed/cDEVWbz2PpQ?si=S4akrfzYJroWiOtB",
+            },
+          ],
+        },
+      ],
+    },
+    2: {
+      notesLink: [
+        {
+          title: "Notes 1",
+          url: "https://firebasestorage.googleapis.com/v0/b/acm-bruh.appspot.com/o/AC%20Module%202.pdf?alt=media&token=e83fe56e-c1e6-4b86-935e-ff49986cad74",
+        },
+      ],
+      topics: [
+        {
+          title: "2.1 MVC in Laravel",
+          description: "Basic concepts and importance",
+          videos: [
+            {
+              title:
+                "Laravel 10 full course for beginner - what is mvc pattern in laravel",
+              url: "https://www.youtube.com/embed/OTzS63zw4cw?si=rWeRQfyw3hpTJEG2",
+            },
+          ],
+        },
+      ],
+    },
   },
-};
+}
 
 export default df;
--- notes-aid/src/notes/ty/it/evenSem/ms/data.tsx
+++ notes-aid/src/notes/ty/it/evenSem/ms/data.tsx
@@ -6,15 +6,105 @@ const ms = {
   icon: Brain,
   color: "blue",
   modules: {
-   1:{
-    notesLink:[
-
-    ],
-    topics:[
-
-    ]
-   }
+    1: {
+      notesLink: [
+        {
+          title: "Notes 1",
+          url: "https://firebasestorage.googleapis.com/v0/b/acm-bruh.appspot.com/o/AC%20Module%201.pdf?alt=media&token=42db56c3-72f1-4758-acb8-8f3ecab36ff3",
+        },
+      ],
+      topics: [
+        {
+          title: "1.4 Single Server Queue",
+          description: "Theoretical based concepts- Important for exam ⭐",
+          videos: [
+            {
+              title:
+                "Simulation Modeling | Tutorial # 37 | Single Server Queuing System (SSQS)",
+              url: "https://www.youtube.com/embed/nDXD8oVelo4?si=AubLWQ_5FszSpAKc",
+            },
+          ],
+        },
+        {
+          title: "1.4 Simple Inventory System",
+          description: "Theoretical based concepts- Important for exam ⭐",
+          videos: [
+            {
+              title: "Ch12-03 Inventory Simulation (Manual)",
+              url: "https://www.youtube.com/embed/JLW-7G4aQNc?si=vglGdcMwMzrcOefb",
+            },
+          ],
+        },
+      ],
+    },
+    2: {
+      notesLink: [
+        {
+          title: "Notes 1",
+          url: "https://firebasestorage.googleapis.com/v0/b/acm-bruh.appspot.com/o/AC%20Module%202.pdf?alt=media&token=e83fe56e-c1e6-4b86-935e-ff49986cad74",
+        },
+      ],
+      topics: [
+        {
+          title: "•	2.1 Lehmer Random Number Generators, Monte Carlo Simulation",
+          description: "Basic concepts and importance",
+          videos: [
+            {
+              title:
+                "SMS#4: LCM (Linear Congruential Method) for random number generation | An example problem",
+              url: "https://www.youtube.com/embed/UBSUTidDJbU?si=muCiw92lTbPSaF-X",
+            },
+          ],
+        },
+        {
+          title: "•	2.2 Empirical Tests of Randomness",
+          description: "Numerical based concepts- Important for exam ⭐",
+          videos: [
+            {
+              title: "K S (Kolmogorov-Smirnov) Test | An example problem",
+              url: "https://www.youtube.com/embed/xdlXSTnB17o?si=GU_yhWnk-dpoOmA2",
+            },
+            {
+              title: "Chi Square Test - Engineering Mathematics 4",
+              url: "https://www.youtube.com/embed/dIoA4CfX8fY?si=AmT3yglNRmnJNNbc",
+            },
+            {
+              title: "Runs Up & Runs Down (Algorithm)",
+              url: "https://www.youtube.com/embed/7TXlY7D-4xQ?si=XRFmRHyQbRGwADY8",
+            },
+            {
+              title: "Runs Up & Down (Solved Problem)",
+              url: "https://www.youtube.com/embed/Ps7RwTgB4HA?si=uBKwfuXWmz99hhSP",
+            },
+            {
+              title: "Runs Above & Below Mean (Algorithm)",
+              url: "https://www.youtube.com/embed/D1lCMy8jmYs?si=zU-WzUzXdOlHN_TT",
+            },
+            {
+              title: "Runs Above & Below Mean (Solved Problem)",
+              url: "https://www.youtube.com/embed/qXepq1dmZS8?si=3qMnC5NU75IpffCX",
+            },
+            {
+              title: "The Gap Test (Algorithm)",
+              url: "https://www.youtube.com/embed/WPAuDQYlQiI?si=J-8LO5-4KennLoIt",
+            },
+            {
+              title: "The Gap Test (Solved Problem)",
+              url: "https://www.youtube.com/embed/xh-4iOv-Oyk?si=JjYWqLASwDGjLdyx",
+            },
+            {
+              title: "Auto-Correlation Test (Algorithm)",
+              url: "https://www.youtube.com/embed/DRY05tx5MTg?si=mNBbMCZL2uiENQan",
+            },
+            {
+              title: "Auto-Correlation Test (Solved Problem)",
+              url: "https://www.youtube.com/embed/SPS3seVeb1A?si=OeQ25GT1Eksqls8w",
+            },
+          ],
+        },
+      ],
+    },
   },
-};
+}
 
 export default ms;
