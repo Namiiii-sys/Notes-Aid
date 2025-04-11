39eeb41357ccf8919a8c26b25841eafab6d801db
Nikunj Rathi <83570245+inikunjrathi@users.noreply.github.com>
2025-04-11 13:04:07 +0530

    completed em (#65)

--- notes-aid/src/notes/fy/comps/evenSem/em/data.tsx
+++ notes-aid/src/notes/fy/comps/evenSem/em/data.tsx
@@ -1,19 +1,326 @@
-import { Brain } from "lucide-react"; 
-
+import { Brain } from "lucide-react";
 
 const em = {
   name: "Engineering Mechanics",
   icon: Brain,
   color: "blue",
   modules: {
-   1:{
-    notesLink:[
-
-    ],
-    topics:[
-
-    ]
-   }
+    1: {
+      notesLink: [
+        {
+          title: "System of Coplanar Forces",
+          url: "https://drive.google.com/file/d/1o1kAwWmh5CPv4lQ25fSvn3uZcNiFzQoK/view?usp=sharing",
+        },
+        {
+          title: "Forces in Space",
+          url: "https://drive.google.com/file/d/1Z5g7wNqQTUoE5B-9pnjfDlGFs1eOzu0C/view?usp=sharing",
+        },
+        {
+          title: "Module 1.1",
+          url: "https://drive.google.com/file/d/1xMqDohgSr2QZWZDz798lrPQaSVy-S6oe/view?usp=sharing",
+        },
+        {
+          title: "Module 1.2",
+          url: "https://drive.google.com/file/d/1RIcAv4gUREOhqn8qNUtvT_QsMOfQdZpJ/view?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "System of coplanar forces",
+          description:
+            "Resultant of concurrent forces, parallel forces, non-concurrent non parallel system of forces, moment of force about a point, couples, Varignon’s theorem, Principle of transmissibility of forces (Vector and analytical approach).",
+          videos: [
+            {
+              title: "Resolution of Forces",
+              url: "https://www.youtube.com/embed/OvbSR3w61lQ?si=92B0CKR8ymPccf0B",
+            },
+            {
+              title: "System and Resultant of Forces",
+              url: "https://www.youtube.com/embed/ROI7H_p8l5U?si=FEpW4IuhLS3f-QzP",
+            },
+            {
+              title:
+                "Principle of Transmissibility of Force and Parallelogram Law of Force",
+              url: "https://www.youtube.com/embed/c-BTpLmPEBo?si=mIJfEF3hVWudthDg",
+            },
+            {
+              title: "Moment and Varignon's Theorem",
+              url: "https://www.youtube.com/embed/Skde9dnFtXQ?si=31qMvB5pzW9epf4N",
+            },
+            {
+              title: "Parallel Force System",
+              url: "https://www.youtube.com/embed/LGC_G7xXWYE?si=khV7GDZkFmPziPRc",
+            },
+            {
+              title: "General Force System Part 1",
+              url: "https://www.youtube.com/embed/-ZInfIGuAOY?si=nwLnlaofRQtG-eSd",
+            },
+            {
+              title: "General Force System Part 2",
+              url: "https://www.youtube.com/embed/6qKqJynU8FM?si=V-Y3rJdJrDsJoBsm",
+            },
+          ],
+        },
+        {
+          title: "Resultant of forces in space.",
+          description: "",
+          videos: [
+            {
+              title: "Space Forces",
+              url: "https://www.youtube.com/embed/dvndNwx_1wU?si=SLkrVflXihSybB3s",
+            },
+          ],
+        },
+      ],
+    },
+    2: {
+      notesLink: [
+        {
+          title: "Kinematics of Particles",
+          url: "https://drive.google.com/file/d/1clzi2-hnHYRxYEP_zN5Gi4ZobYA3uw0b/view?usp=sharing",
+        },
+        {
+          title: "Kinematics of Rigid Bodies",
+          url: "https://drive.google.com/file/d/1iHmZv2awONO7fhmzk9_U5-sUTB439KwO/view?usp=sharing",
+        },
+        {
+          title: "Module 2.1",
+          url: "https://drive.google.com/file/d/1VbWlHkbwWwgiOtGxQDmxQ_wpOSuLLr0P/view?usp=sharing",
+        },
+        {
+          title: "Module 2.2",
+          url: "https://drive.google.com/file/d/12ar97ItNHO2OWc1hEfYQeCYjwcK3-AZb/view?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "Kinematics of Particles",
+          description:
+            "Variable motion, motion curves (a-t, v-t, s-t) (acceleration curves restricted to linear acceleration only), motion along plane curved path, velocity & acceleration in terms of rectangular components, tangential & normal component of acceleration",
+          videos: [
+            {
+              title: "Introduction",
+              url: "https://www.youtube.com/embed/gRbmwCQcy64?si=pjadX5GEZA_kjfz6",
+            },
+            {
+              title: "Motion Under Gravity",
+              url: "https://www.youtube.com/embed/UVf87irzUxE?si=E_sSU_dhakZkF0s4",
+            },
+            {
+              title: "Variable Acceleration",
+              url: "https://www.youtube.com/embed/iroL-SsK_D4?si=67_1OR0xNWNn-471",
+            },
+            {
+              title: "Curvilinear Motion Rectangular System",
+              url: "https://www.youtube.com/embed/kn-6HBdo8Xk?si=q30RQvenGzOwxna0",
+            },
+            {
+              title: "Numerical of Curvilinear Rectangular System",
+              url: "https://www.youtube.com/embed/fbb3C_FKDzs?si=hOnlOJYPAlRPV3bL",
+            },
+            {
+              title: "Tangential and Normal Components of Acceleration",
+              url: "https://www.youtube.com/embed/l-Xvy-MmOsU?si=IN1zNauJYeuu2yes",
+            },
+          ],
+        },
+        {
+          title: "Kinematics of Rigid Bodies",
+          description:
+            " Introduction to general plane motion, problems based on ICR method for general plane motion of bodies (up to 2 linkage mechanism and no relative velocity method).",
+          videos: [
+            {
+              title: "General Plane Motion",
+              url: "https://www.youtube.com/embed/Sg11QkyY_Vs?si=F3uaFp9vPx8SnNCw",
+            },
+            {
+              title: "Numerical on Instantaneous Center of Rotation Part 1",
+              url: "https://www.youtube.com/embed/M84RLi0FrnE?si=kCCj-k83ALpzIt4Q",
+            },
+            {
+              title: "Numerical on Instantaneous Center of Rotation Part 2",
+              url: "https://www.youtube.com/embed/3aN0ptMiONM?si=438oEz00BQdN1emX",
+            },
+          ],
+        },
+      ],
+    },
+    3: {
+      notesLink: [
+        {
+          title: "Centroid",
+          url: "https://drive.google.com/file/d/1QXyzQh4F0iXluY54KnjOFq_eJ_i8cC6Z/view?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "Centroid",
+          description: "",
+          videos: [
+            {
+              title: "Centroid of Area Part 1",
+              url: "https://www.youtube.com/embed/8Z74kpzFC1k?si=TYPIB3Rhz9B-qK9Y",
+            },
+            {
+              title: "Centroid of Area Part 2",
+              url: "https://www.youtube.com/embed/Ya_jNs3fEv4?si=wop6MnjCgyIN0HzO",
+            },
+            {
+              title: "Centroid of Lamina Part 1",
+              url: "https://www.youtube.com/embed/xkKTHGBQaUA?si=rSmcAWLrJ0bhbDX7",
+            },
+            {
+              title: "Centroid of Lamina Part 2",
+              url: "https://www.youtube.com/embed/46CP5tbBcF8?si=2TT0nnUiUElpX2c0",
+            },
+          ],
+        },
+      ],
+    },
+    4: {
+      notesLink: [
+        {
+          title: "Eqiolibrium of Force Systems",
+          url: "https://drive.google.com/file/d/1A4A95Gj6NFOG8sL0aJsZG7y_MXozkUe1/view?usp=sharing",
+        },
+        {
+          title: "Friction",
+          url: "https://drive.google.com/file/d/1ZFgJ-MqliWC0vQeoqwqzUdqkxeAtYuPH/view?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title:
+            "Equilibrium of system of coplanar forces: Condition of equilibrium for concurrent forces, parallel forces and non-concurrent, non-parallel force system (general force system), Free body diagram.",
+          description: "Refer notes for this part",
+        },
+        {
+          title: "Support, Leads and Beams",
+          description:
+            "Types of support, loads, beams, determination of reactions at supports for various types of loads on beams (excluding internal hinge and compound beam problems).",
+          videos: [
+            {
+              title: "Types of support and loads",
+              url: "https://www.youtube.com/embed/wunoaGShCKQ?si=UtBApF8PqVal9r9g",
+            },
+            {
+              title: "Numerical on Uniformly Distributed Load",
+              url: "https://www.youtube.com/embed/l1MoKLunG9A?si=Ofl15BFzW-ovRGF0",
+            },
+            {
+              title: "Numerical on Uniformly Varying Load",
+              url: "https://www.youtube.com/embed/hNl9V0GvhX8?si=xFICWLNykpkaxr_q",
+            },
+            {
+              title: "Lami's Theorem",
+              url: "https://www.youtube.com/embed/BTxbpRUnjqk?si=laUcakNoJSxSkW_F",
+            },
+            {
+              title: "Connected Spheres (Refer notes for connected bodies)",
+              url: "https://www.youtube.com/embed/XzH53z9SwSE?si=bSDoP2UO5wp5LHxz",
+            },
+          ],
+        },
+        {
+          title: "Friction",
+          description:
+            "Laws of friction, cone of friction, angle of repose, equilibrium of bodies on inclined plane, application to problems involving wedges and ladders.",
+          videos: [
+            {
+              title: "Introduction to Friction",
+              url: "https://www.youtube.com/embed/DqNflziPGJM?si=x4xs-eGUxnPGfiPd",
+            },
+            {
+              title: "Angles involved in Friction",
+              url: "https://www.youtube.com/embed/CjgHMa66gKk?si=Y-CoJcttS0mYEdP6",
+            },
+            {
+              title: "Numericals on Simple Friction",
+              url: "https://www.youtube.com/embed/30FZvlpHG80?si=On-RMsk9IciBi7SU",
+            },
+            {
+              title: "Numericals on Simple Friction on Connected Block",
+              url: "https://www.youtube.com/embed/NBwBUu1Qbb8?si=xizU1cEb-LVUhrrX",
+            },
+            {
+              title: "Numericals on Wedge Part 1",
+              url: "https://www.youtube.com/embed/6wG00oXJoRI?si=vAVt1obzjU9fROfG",
+            },
+            {
+              title: "Numericals on Wedge Part 2",
+              url: "https://www.youtube.com/embed/RJwpjX6PIl0?si=x-rDXwFGG6xgB2cl",
+            },
+            {
+              title: "Numericals on Ladder Part 1",
+              url: "https://www.youtube.com/embed/pbGKWCjVHgo?si=hvKhNSgew9DBVBdT",
+            },
+            {
+              title: "Numericals on Ladder Part 2",
+              url: "https://www.youtube.com/embed/TytdVNn5xxg?si=ztbbkyoHbPA9WBNS",
+            },
+          ],
+        },
+      ],
+    },
+    5: {
+      notesLink: [
+        {
+          title: "Newton's Second Law",
+          url: "https://drive.google.com/file/d/1HkUs7-LTafoHeOo2fD7pUi-Ij3E6kQrx/view?usp=sharing",
+        },
+        {
+          title: "Work Energy Principle",
+          url: "https://drive.google.com/file/d/1_8VCm_QwZkoFS5tXvVxF3kwTPfG1u_4R/view?usp=sharing",
+        },
+        {
+          title: "Impulse Momentum Equation",
+          url: "https://drive.google.com/file/d/1ymoSOgtJD9SQ-l592JcdDlBeZoLAmRX-/view?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "Force and acceleration.",
+          description:
+            "Introduction to basic concepts, equations of dynamic equilibrium, Newton's second law of motion (only rectilinear motion).",
+          videos: [
+            {
+              title: "Newton's Second Law of Motion and D'Alembert's Principle",
+              url: "https://www.youtube.com/embed/Im7iPOIMTH4?si=VLHR_OsCnmEVGLHd",
+            },
+          ],
+        },
+        {
+          title: "Work energy principle.",
+          description:
+            "Work done by forces, work energy equation for rectilinear motion.",
+          videos: [
+            {
+              title: "Work Energy Principle",
+              url: "https://www.youtube.com/embed/x0t5jnL8juk?si=1lhDf1iVraqIv8qx",
+            },
+            {
+              title: "Numericals on Work Energy Principle",
+              url: "https://www.youtube.com/embed/bku4ZsMe_KI?si=zkWBnWYr9kP0PMh7",
+            },
+          ],
+        },
+        {
+          title: "Impulse and Momentum.",
+          description:
+            "Principle of linear impulse and momentum, law of conservation of momentum, impact and collision, direct central and oblique central impact.",
+          videos: [
+            {
+              title: "Impulse Momentum Method",
+              url: "https://www.youtube.com/embed/vHWvUeiFrJA?si=bz6GVM_5xmmg_T5Y",
+            },
+            {
+              title: "Numericals on Impact",
+              url: "https://www.youtube.com/embed/6kxYVI6mIuI?si=zHY7NpR0kwBC055D",
+            },
+          ],
+        },
+      ],
+    },
   },
 };
 
