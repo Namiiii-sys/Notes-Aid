5a8025f2c6e12bd392c5a0b4f250298b6a0a0741
Atharva Sawant <47493392+AtharvaSawant@users.noreply.github.com>
2025-04-14 21:37:14 +0530

    ED Version 1 (#71)

--- notes-aid/src/notes/fy/comps/oddSem/ed/data.tsx
+++ notes-aid/src/notes/fy/comps/oddSem/ed/data.tsx
@@ -1,13 +1,269 @@
 import { Brain } from "lucide-react";
-
 const ed = {
   name: "Engineering Drawing",
   icon: Brain,
   color: "blue",
   modules: {
     1: {
-      notesLink: [],
-      topics: [],
+      notesLink: [
+        {
+          title: "Projection of Points lines and planes",
+          url: "https://drive.google.com/drive/folders/1NNtOEAd-dMCIf78W6bbu0NxtvvNsXISp?usp=share_link",
+        },
+        {
+          title: "Projection of PLP Drawings",
+          url: "https://drive.google.com/drive/folders/1Y59zzKsQqPMC1CmyGVUrMLhQfTWPzvaH?usp=share_link",
+        },
+        {
+          title: "AutoCad File Projection of Lines",
+          url: "https://drive.google.com/file/d/1VJ0EISC5V2Y6UwBr5dFPF_SQuVqZpa_b/view?usp=share_link",
+        },
+        {
+          title: "AutoCad File Projection of Planes",
+          url: "https://drive.google.com/file/d/1R3RsRtHZfFsRNVG8Ej7KZ9YXGMOpipH3/view?usp=share_link ",
+        },
+      ],
+      topics: [
+        {
+          title: "Understading Engineering Drawing",
+          description: "Basics of Engineering Drawing",
+          videos: [
+            {
+              title: "Understading Engineering Drawing",
+              url: "https://www.youtube.com/embed/ht9GwXQMgpo?si=BHcs9DzqweFbj5CH",
+            },
+          ]
+        },
+        {
+          title: "Introduction to Engineering Drawings",
+          description: "1.1",
+          videos: [
+            {
+              title: "Things to Carry",
+              url: "https://www.youtube.com/embed/FQhGBy0-tYw?si=aTrV94S6DoKHNd5s",
+            },
+          ]
+        },
+        {
+          title: "Projections of Point,Lines and Planes",
+          description: "1.2",
+          videos: [
+            {
+              title: "Projections of Lines and Planes",
+              url: "https://www.youtube.com/embed/aKg5q3rLEdg?si=F5ppUXnTEzYL6udx" ,
+            },
+            {
+              title:"Projection of Planes",
+              url:"https://www.youtube.com/embed/dJD-1a3FMyU?si=hyyd_2eg2yf65sB5",
+            }
+          ]
+        },
+      ]
+    },
+    2: {
+      notesLink: [
+        {
+          title: "Orthogonal Projections",
+          url: "https://drive.google.com/drive/folders/your-new-folder-link?usp=share_link",
+        },
+        {
+          title: "Orthogonal Projections Drawings",
+          url: "https://drive.google.com/drive/folders/1-7ov3ehTncWBHMJh1GZ1jmQiNz7GNa3I?usp=share_link",
+        },
+        {
+          title: "AutoCad File Sectional Orthographic Assignment",
+          url: "https://drive.google.com/file/d/158XWzg5MFHGz5Ogt1orNSRQhQCm8PHRR/view?usp=share_link",
+        }
+      ],
+      topics:[
+        {
+          title: "Orthogonal Projection of Solids",
+          description: "2.1",
+          videos: [
+            {
+              title: "Introductions to Orthogonal Projections",
+              url: "https://www.youtube.com/embed/bVy-_iqLo1k?si=nkYYTdNKoqLDuvcR&amp;controls=0",
+            }
+          ]
+        },
+        {
+          title:"Orthographic Projection of Solids",
+          description:"2.2",
+          videos:[
+            {
+              title:"Sectional Orthographic Projections",
+              url:"https://www.youtube.com/embed/LLREqqFov2c?si=CYzaKXFOHOVsZPZz",
+            }
+          ]
+          
+        }
+      ]
+    },
+    3: {
+      notesLink: [
+        {
+          title: "Isometric Views",
+          url: "https://drive.google.com/drive/folders/1rYpucN_OZZZRNOtlhjuIRxcS20nERXdM?usp=share_link",
+        },
+      ],
+      topics: [
+        {
+          title:"Isometric Views",
+          description:"3.1",
+          videos:[
+            {
+              title:"Introduction to Isometric Views",
+              url:"https://www.youtube.com/embed/8j7l1OWhMlE?si=OB9PhwM1bgg5wXF4",
+            }
+          ]
+        },
+        {
+          title:"Isometric Views",
+          description:"3.2",
+          videos:[
+            {
+              title:"How to construct a Isometric View of an Object",
+              url:"https://www.youtube.com/embed/zKFAbmnUvGU?si=tdA_VUEtS86Bvwgq",  
+            }
+          ]
+        },
+        {
+          title: "Quetions on Isometric Views",
+          description: "Draw Isometric View",
+          videos: [
+            {
+              title: "Q1-Draw Isometric View of the following.",
+              url: "https://www.youtube.com/embed/qvfBXHrIaWE?si=RYEhmgi7JN31Nt1z",
+            },
+            {
+              title: "Q2- Given Front View and Top View",
+              url: "https://www.youtube.com/embed/WSME3_4sLoA?si=42aYrEvmcxmib3aL",
+            },
+            {
+              title: "Q3- Given Front View and Top View ",
+              url: "https://www.youtube.com/embed/yF2hsEr7AI8?si=PKoznNIwSgGJtX0y",
+            },
+            {
+              title: "Q4- Given Front View and Top View",
+              url: "https://www.youtube.com/embed/vqvJ0RK32Es?si=Sk09S7p-W15_TX06",
+            },
+            {
+              title: "Q5- Given Front View and Top View",
+              url: "https://www.youtube.com/embed/caWBryWxXBM?si=WNe6eViftS5n7yaI",
+            },
+            {
+              title: "Q6- Given Top View and Left Side View",
+              url: "https://www.youtube.com/embed/jys3XcdLdWw?si=WsbRH6IuBNrcq_hO",
+            },
+            {
+              title: "Q7- Given Front View and Top View",
+              url: "https://www.youtube.com/embed/HiAAyZz24oA?si=nPTtb8wvJxdDjBvy",
+            },
+            {
+              title: "Q8- Given Front View and Top View",
+              url: "https://www.youtube.com/embed/RjpQMnK0w2Y?si=vJZlhdfqZO0BcJwM",
+            }
+          ]
+        }
+      ],
+    },
+    4: {
+      notesLink: [
+        {
+          title: "Projections of Solids",
+          url: "https://drive.google.com/drive/folders/1spCNaeG5ir19Myhy2FK1NuVgVBg_-4y5?usp=share_link",
+        },
+        {
+          title: "Projections of Solids Drawings",
+          url: "https://drive.google.com/drive/folders/13dVBvOdLJ8OrdrwR-KuSfTCXZ0gGDD_Y?usp=share_link",
+        },
+        {
+          title: "AutoCad File - Projections of Solids ",
+          url: "https://drive.google.com/file/d/1H9vVteanjDcM0vIFPq64q4gwPiebp8A5/view?usp=share_link",
+        },
+        {
+          title:"AutoCad File- Projection of Cones",
+          url:"https://drive.google.com/file/d/1H9vVteanjDcM0vIFPq64q4gwPiebp8A5/view?usp=share_link",
+        }
+      ],
+      topics: [
+        {
+          title:"Projection of Solids",
+          description:"4.1",
+          videos: [
+            {
+              title: "True Shape Orientation",
+              url: "https://www.youtube.com/embed/zi7XYYoqg9E?si=1Vxeokc7f2qjNKW6",
+            },
+            {
+              title: "Projection of Square Pyramid",
+              url: "https://www.youtube.com/embed/Ht922zIOE1k?si=l0bAc50sbrhei9Yg", 
+            }
+          ]
+        },
+        
+        {
+          title: "Projection of Solids",
+          description: "4.2",
+          videos: [
+            {
+              title: "Projection of Square Pyramid",
+              url: "https://www.youtube.com/embed/Ht922zIOE1k?si=l0bAc50sbrhei9Yg", 
+            },
+            {
+              title: "Projection of Solids-Cones",
+              url: "https://www.youtube.com/embed/iCqxpcsqBLg?si=UxACv742L81VP1Ze",
+            },
+          ]
+        }
+      ],
+    },
+    5: {
+      notesLink: [
+        {
+          title: "Section and Development of Solids",
+          url: "https://drive.google.com/drive/folders/1XepFjoOrg7LvjsomK0GJNxSICthx1QFc?usp=share_link",
+        },
+        {
+          title: "Section and Development of Solids Drawings",
+          url: "https://drive.google.com/drive/folders/1oIxlMFmWhV7noTTfRTFA7votS2-DZxS2?usp=share_link",
+        },
+        {
+          title: "AutoCad- Section and Development of Solids Part 1",
+          url: "https://drive.google.com/file/d/1tdUeXsgJwSQ9hB9UlHCTf4xdv-UHbAmb/view?usp=share_link",
+        },
+        {
+          title: "AutoCad- Section and Development of Solids Part 2",
+          url: "https://drive.google.com/file/d/1b5PYhS8UFSTc7cVdCguNH1soTi5G20Sx/view?usp=share_link",
+        },
+
+      ],
+      topics: [
+        {
+          title: "Sectional and Development of Solids",
+          description: "5.1",
+          videos: [
+            {
+              title: "Sectional Orthographic Projections",
+              url: "https://www.youtube.com/embed/4ATWYImE6U4?si=ymlf_ENPg4PfArJM",
+            },
+            {
+              title: "Sectional of Petagon Prism",
+              url:"https://www.youtube.com/embed/A7RuvwMXdSI?si=P2I4Eb6CbdHZJ_pt" ,
+            },
+          ]
+        },
+        {
+          title:"Lateral Surfaces of Solids",
+          description:"5.2",
+          video:[
+            {
+              title:"Lateral Surfaces of Cone",
+              url:"https://www.youtube.com/embed/uKvZB_fjMxs?si=bE3Rj7rt2-ZpqRFC",
+            },
+          ]
+        }
+      ],
     },
   },
 };
