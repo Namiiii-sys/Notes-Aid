3499621eb07c7fd1e78ad6798eb606611c594d83
romill09 <romil.code09@gmail.com>
2025-04-25 23:22:25 +0530

    notify: Added TY AC mod 3 & 4 videos

--- notes-aid/src/notes/ty/comps/evenSem/ac/data.tsx
+++ notes-aid/src/notes/ty/comps/evenSem/ac/data.tsx
@@ -257,107 +257,245 @@ const ac = {
       ],
     },
     3: {
-      notesLink: [{
-        title: "Module 3 PPT",
-        url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/rohit_sd_somaiya_edu/EYOSd9pg17BHkiLudQNLrWQBIlM-O7e-6mhANAJ3eZbA7g?e=OPI4O6",
-      }
+      notesLink: [
+        {
+          title: "Module 3 PPT",
+          url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/rohit_sd_somaiya_edu/EYOSd9pg17BHkiLudQNLrWQBIlM-O7e-6mhANAJ3eZbA7g?e=OPI4O6",
+        },
       ],
 
       topics: [
         {
-          title: "3.1 Mathematics of Asymmetric Key Cryptography: Primes,Primality Testing, Factorization, Quadratic Congruence,Exponentiation and Logarithm",
+          title:
+            "3.1 Mathematics of Asymmetric Key Cryptography: Primes,Primality Testing, Factorization, Quadratic Congruence,Exponentiation and Logarithm",
           description: "Theoretical based concepts",
           videos: [
-
+            {
+              title: "Prime Factorization",
+              url: "https://www.youtube.com/embed/tKTNVmnW_4w?si=XvXmb4gvEH8tJ66G",
+            },
+            {
+              title: "Testing for Primality (Feramt's Test)",
+              url: "https://www.youtube.com/embed/sDrXeCs3ghQ?si=2zCUb2yHuAAOfn9T",
+            },
+            {
+              title: "Testing for Primality (Miller-Rabin Test)",
+              url: "https://www.youtube.com/embed/8i0UnX7Snkc?si=uyR6CVDSyO3z9Xgh",
+            },
+            {
+              title: "Euler's Totient Function",
+              url: "https://www.youtube.com/embed/DwQ7-k9LkJ4?si=N8ZggdjDGlfchLCl",
+            },
+            {
+              title: "Fermat's Little Theorem",
+              url: "https://www.youtube.com/embed/3Cb0ys-jppU?si=zZtaC_OfR3yaZsxM",
+            },
+            {
+              title: "Euler's Theorem",
+              url: "https://www.youtube.com/embed/DyOv20d4c70?si=uCDvRgE5ZFisNvDv",
+            },
+            {
+              title: "Chinese Remainder Theorem - Solved Example 1",
+              url: "https://www.youtube.com/embed/e8DtzQkjOMQ?si=om4Hg88r6iOp1uxE",
+            },
+            {
+              title: "Chinese Remainder Theorem - Solved Example 2",
+              url: "https://www.youtube.com/embed/zd1_iY0FSEo?si=uoMvTES8s8mauyj",
+            },
           ],
         },
         {
-          title: "3.2 Public key cryptography: Principles of public key cryptosystems, The RSA algorithm, attacks on RSA",
+          title:
+            "3.2 Public key cryptography: Principles of public key cryptosystems, The RSA algorithm, attacks on RSA",
           description: "Theoretical based concepts",
           videos: [
-
-          ]
+            {
+              title:
+                "Symmetric Key Cryptography Vs Asymmetric Key Cryptography",
+              url: "https://www.youtube.com/embed/H7w4ZmaFzfg?si=oAb6qrVSFx-ThOzI",
+            },
+            {
+              title: "Asymmetric key Cryptography with example",
+              url: "https://www.youtube.com/embed/xw19eT5thIE?si=Gppp1aYyf5YV4jOw",
+            },
+            {
+              title: "Asymmetric Key Cryptography: RSA",
+              url: "https://www.youtube.com/embed/rlJTMUBXhKE?si=SSda6Hhhiy5EW52u",
+            },
+            {
+              title: "Attacks on RSA",
+              url: "https://www.youtube.com/embed/cC8kKIvve-M?si=E0iaSDRp9DcYPX1v",
+            },
+          ],
         },
         {
-          title: "3.3 Key management: Diffie Hellman Key exchange, Man-in Middle attack",
+          title:
+            "3.3 Key management: Diffie Hellman Key exchange, Man-in Middle attack",
           description: "Theoretical based concepts",
           videos: [
-
-          ]
-        }
-      ]
+            {
+              title: "Diffie Hellman Key Exchange Algorithm: Primitive Roots",
+              url: "https://www.youtube.com/embed/DKy98FWHwdg?si=ttUsWITWDDug3wXC",
+            },
+            {
+              title: "Diffie Hellman Key Exchange Algorithm",
+              url: "https://www.youtube.com/embed/wcSyOZJf7Hw?si=bzppUM5LI6pYYCg7",
+            },
+            {
+              title: "Man in the middle attack in Diffie Hellman Key Exchange",
+              url: "https://www.youtube.com/embed/ev2Cbu23m-Q?si=UvJ3CRRrSGkcfwdG",
+            },
+          ],
+        },
+      ],
     },
     4: {
-
-      notesLink: [{
-        title: "Module 4 PPT",
-        url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/rohit_sd_somaiya_edu/EdBTyHnqCptPvtz0QGSMkK8BZFx5yvsuF942X51OojkE1g?e=VT6uDK"
-      }
-
+      notesLink: [
+        {
+          title: "Module 4 PPT",
+          url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/rohit_sd_somaiya_edu/EdBTyHnqCptPvtz0QGSMkK8BZFx5yvsuF942X51OojkE1g?e=VT6uDK",
+        },
       ],
       topics: [
         {
-          title: "4.1 4.1 Message Authentication Approaches, Hash Function, Cryptographic Hash Function Requirements, Cryptographic Hash Function Security, Cryptographic Hash Function Structure, SHA, HMAC, MD5.",
+          title:
+            "4.1 Message Authentication Approaches, Hash Function, Cryptographic Hash Function Requirements, Cryptographic Hash Function Security, Cryptographic Hash Function Structure, SHA, HMAC, MD5.",
           description: "Theoretical based concepts",
           videos: [
-
-          ]
+            {
+              title: "Message Authentication Approaches",
+              url: "https://www.youtube.com/embed/SAz7-A6rNKM?si=O8CcqsQAAgCCdc4t",
+            },
+            {
+              title: "Hash Function",
+              url: "https://www.youtube.com/embed/jNoUtbK8hv4?si=MFvw1p8VVnnj0IA",
+            },
+            {
+              title: "Cryptographic Hash Function Properties Part 1",
+              url: "https://www.youtube.com/embed/kGFJAjLuts4?si=NfGH_u6O9QwOmdzl",
+            },
+            {
+              title: "Cryptographic Hash Function Properties Part 2",
+              url: "https://www.youtube.com/embed/Q2H2ndbHUFQ?si=yZZgY0s0mD2BXIz_",
+            },
+            {
+              title: "Attacks on hash functions (Birthday Paradox) Part 1",
+              url: "https://www.youtube.com/embed/OcCRflNM-Xk?si=dUtuQHnkLvpHzTFL",
+            },
+            {
+              title: "Attacks on hash functions (Birthday Paradox) Part 2",
+              url: "https://www.youtube.com/embed/2bEL3ok8D70?si=l1SO4iuy6FS5jZDB",
+            },
+            {
+              title: "Secure Hash Algorithm (SHA-512)",
+              url: "https://www.youtube.com/embed/VtHSyoJkDXw?si=MjnZ3VHgJsGLl77q",
+            },
+            {
+              title: "Hash based Message Authentication Code (HMAC)",
+              url: "https://www.youtube.com/embed/sxWxCtJ3og0?si=5hT2tbI7zW2uEi8P",
+            },
+            {
+              title: "Message Digest 5 (MD5)",
+              url: "https://www.youtube.com/embed/af3ORsj_k7w?si=N94nJxoz_TrQj7Sj",
+            },
+            {
+              title: "Applications of hash functions",
+              url: "https://www.youtube.com/embed/KexR9ylLLcQ?si=L9pRYZuNMbHfMCYf",
+            },
+            {
+              title: "Requirements & security of hash functions",
+              url: "https://www.youtube.com/embed/vDQLPN-XTWE?si=-aRHsRLXo4jlLksS",
+            },
+          ],
         },
         {
-          title: "4.2 Using Symmetric Encryption for Message Authentication, Message Authentication Code (MAC), Digital Authentication Algorithm (DAA)",
+          title:
+            "4.2 Using Symmetric Encryption for Message Authentication, Message Authentication Code (MAC), Digital Authentication Algorithm (DAA)",
           description: "Theoretical based concepts",
           videos: [
-
-          ]
+            {
+              title: "Message Authentication Code (MAC))",
+              url: "https://www.youtube.com/embed/Om4O14irGzA?si=2FWybMFubVRkl4sz",
+            },
+            {
+              title: "Digital Authentication Algorithm (DAA)",
+              url: "https://www.youtube.com/embed/PH61OkF6Xts?si=bMDx90lB2xa8sRxv",
+            },
+          ],
         },
         {
-          title: "4.3 Using Public Key for Authentication, Digital Signatures, Properties of Digital Signatures beyondMessage Authentication, DSS, Authentication Applications: Kerberos, X.509 Authentication Service",
+          title:
+            "4.3 Using Public Key for Authentication, Digital Signatures, Properties of Digital Signatures beyondMessage Authentication, DSS, Authentication Applications: Kerberos, X.509 Authentication Service",
           description: "Theoretical based concepts",
           videos: [
-
-          ]
-        }
-      ]
-
+            {
+              title: "Digital Signatures",
+              url: "https://www.youtube.com/embed/y6e70OKHg7c?si=weQoOwNGhJKTypIf",
+            },
+            {
+              title: "Properties of Digital Signatures",
+              url: "https://www.youtube.com/embed/bM8lZaFN2gw?si=m-4TtaPNeoAQi5KF",
+            },
+            {
+              title:
+                "DSS signatures, RSA Signatures, Digital Signature Algorithm",
+              url: "https://www.youtube.com/embed/eiNZi-G-CE4?si=DzBUKT2RPwUHYOwd",
+            },
+            {
+              title: "Digital Signature scheme RSA",
+              url: "https://www.youtube.com/embed/jXw-CXYT01M?si=UtnEcRCawGylxh9T",
+            },
+            {
+              title: "Digital signature schemes ElGamal & Schnorr",
+              url: "https://www.youtube.com/embed/kuiVid1G8E8?si=e-GaTHmzLBewz60k",
+            },
+            {
+              title: "X.509 Authentication Service",
+              url: "https://www.youtube.com/embed/sYSO13LRfYA?si=ohJqBtgEuEAl1S-p",
+            },
+            {
+              title: "Kerberos Part 1",
+              url: "https://www.youtube.com/embed/OuJe0d1NGaM?si=4tHpH8kWfgOxKyl7",
+            },
+            {
+              title: "Kerberos Part 2",
+              url: "https://www.youtube.com/embed/p6w9RZ5PNho?si=STV08bb8GQo52tki",
+            },
+          ],
+        },
+      ],
     },
     5: {
       notesLink: [
         {
-          title:"Module 5 PPT",
-          url:"https://somaiya0-my.sharepoint.com/:p:/g/personal/minav_karia_somaiya_edu/EdV9HQ_ccb5EuXJCAtEAEy0BlAn0pWUJ3wyxvepNCl8UlQ?e=YwXBPD"
-        }
+          title: "Module 5 PPT",
+          url: "https://somaiya0-my.sharepoint.com/:p:/g/personal/minav_karia_somaiya_edu/EdV9HQ_ccb5EuXJCAtEAEy0BlAn0pWUJ3wyxvepNCl8UlQ?e=YwXBPD",
+        },
       ],
       topics: [
         {
           title: "5.1 Quantum Cryptography, Quantum key distribution-QKD",
           description: "Theoretical based concepts",
-          videos: [
-
-          ]
+          videos: [],
         },
         {
           title: "5.2 Homomorphic Encryption",
           description: "Theoretical based concepts",
-          videos: [
-
-          ]
+          videos: [],
         },
         {
-          title: "5.3 Secure Multi-Party Computation (MPC) In particular,Zero-Knowledge Proofs",
+          title:
+            "5.3 Secure Multi-Party Computation (MPC) In particular,Zero-Knowledge Proofs",
           description: "Theoretical based concepts",
-          videos: [
-
-          ]
+          videos: [],
         },
         {
           title: "5.4 Cryptographic Obfuscation",
           description: "Theoretical based concepts",
-          videos: [
-          ]
-        }
-      ]
-    }
-
+          videos: [],
+        },
+      ],
+    },
   },
 };
 
