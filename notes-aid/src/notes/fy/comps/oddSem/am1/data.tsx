bc9e6a403a7bc6ef8b0079e3bba0caa8ff7c86bc
Samridha Das <samridha2020@gmail.com>
2025-05-19 18:45:21 +0530

    Add Am-1 Notes (#93)

--- notes-aid/src/notes/fy/comps/oddSem/am1/data.tsx
+++ notes-aid/src/notes/fy/comps/oddSem/am1/data.tsx
@@ -6,10 +6,161 @@ const am1 = {
   color: "blue",
   modules: {
     1: {
-      notesLink: [],
-      topics: [],
+      notesLink: [
+        {
+          title: "MOD-1 Notes",
+          url: "https://drive.google.com/drive/folders/1Eikm_-bjyrV8n9d0UeNq-GvI1_KdnL95?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "1.1 Statement of De Moivre's Theorem & Related Examples",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "1.2 Powers and roots of complex numbers",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "1.3 Circular functions and hyperbolic functions of complex number",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "1.4 Inverse circular and inverse hyperbolic functions",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "1.5 Logarithm of complex numbers",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "1.6 Separation of real and imaginary parts of a function",
+          description: "",
+          videos: [],
+        },
+      ],
     },
-  },
+    2: {
+      notesLink: [
+        {
+          title: "MOD-2 Notes",
+          url: "https://drive.google.com/drive/folders/16BMER-kmLog1enTHb-Yt2Ti_wjwIVB8-?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "2.1 Types of matrices",
+          description: "Hermitian, Skew-Hermitian, Unitary and Orthogonal matrix",
+          videos: [],
+        },
+        {
+          title: "2.2 Rank of a matrix using row echelon forms, reduction to normal form",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "2.3 System of homogeneous and non-homogeneous equations, their consistency and solutions",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "2.4 Linearly dependent and independent vectors",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "2.5 Solution of system of linear algebraic equations by (a) Gauss Seidal method (b) Jacobi iteration method",
+          description: "",
+          videos: [],
+        },
+      ],
+    },
+    3: {
+      notesLink: [
+        {
+          title: "MOD-3 Notes",
+          url: "https://drive.google.com/drive/folders/16iFeqFgYJJqeOSq-la5j0KlVSjMT3eNw?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "3.1 Functions of several variables, Partial derivatives of first and higher order",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "3.2 Differentiation of composite functions",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "3.3 Maxima and minima of a function of two independent variables",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "3.4 Introduction of Jacobian of two and three independent variables",
+          description: "",
+          videos: [],
+        },
+      ],
+    },
+    4: {
+      notesLink: [
+        {
+          title: "MOD-4 Notes",
+          url: "https://drive.google.com/drive/folders/1kOU08yQ8Qv5CDCgsG-yXQVLFj9wdb2Rc?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "4.1 Euler’s theorem on homogeneous functions with two and three independent variables",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "4.2 Deductions(Corollaries) from Euler’s theorem (statements only) and problems",
+          description: "",
+          videos: [],
+        },
+      ],
+    },
+    5: {
+      notesLink: [
+        {
+          title: "MOD-5 Notes",
+          url: "https://drive.google.com/drive/folders/1yzaGKBMGBxCKerHjnodvoxgY36ET90Zr?usp=sharing",
+        },
+      ],
+      topics: [
+        {
+          title: "5.1 Differential Equation of first order and first degree",
+          description: "Exact differential equations, Equations reducible to exact equations by integrating factors",
+          videos: [],
+        },
+        {
+          title: "5.2 Linear differential equations (Review), Equation reducible to linear form. Applications of Differential Equation of first order and first degree",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "5.3 Linear Differential Equation with constant coefficients",
+          description: "",
+          videos: [],
+        },
+        {
+          title: "5.4 Method of variation of parameters",
+          description: "",
+          videos: [],
+        },
+      ],
+    }, 
+  }
 };
 
 export default am1;
